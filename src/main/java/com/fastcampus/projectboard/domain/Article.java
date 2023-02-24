8cc0563807d8a67e83cc9eebdd093e6ab35bf940
keemsora <itsyoursora@gmail.com>
2023-02-24 18:33:15 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블로 매칭이 되도록 설계
    생성자, `equals()`, `hashCode()` 등이 도메인 맥락에 맞게끔 디테일하게 설계

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,20 +1,92 @@
 package com.fastcampus.projectboard.domain;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
-import javax.servlet.http.Cookie;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+	@Index(columnList = "title"),
+	@Index(columnList = "hashtag"),
+	@Index(columnList = "createdAt"),
+	@Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String title;	// 제목
-	private String content;	// 본문
-	private String hashtag;	// 해시태그
-
-	private LocalDateTime createdAt;	// 생성일시
-	private String createdBy;	// 생성자
-	private LocalDateTime modifiedAt;	// 수정일시
-	private String modifiedBy;	// 수정자
-	
 
 
+	@Setter @Column(nullable = false) private String title;	// 제목
+	@Setter @Column(nullable = false, length = 10000) private String content;	// 본문
+
+	@Setter private String hashtag;	// 해시태그
+
+	@ToString.Exclude
+	@OrderBy("id")
+	@OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+	private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+	@CreatedDate @Column(nullable = false) private LocalDateTime createdAt;	// 생성일시
+	@CreatedBy @Column(nullable = false, length = 100) private String createdBy;	// 생성자
+	@LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;	// 수정일시
+	@LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;	// 수정자
+
+
+	// JPA 엔티티들은 기본 생성자를 가지고 있어야 한다.
+	// 평소에는 오픈하지 않을 것이기 때문에 protected로 설정한다.
+	protected Article() {}
+
+	private Article(String title, String content, String hashtag) {
+		this.title = title;
+		this.content = content;
+		this.hashtag = hashtag;
+	}
+
+	public static Article of(String title, String content, String hashtag) {
+		return new Article(title, content, hashtag);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Article article = (Article)o;
+		return id != null && id.equals(article.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
 }
